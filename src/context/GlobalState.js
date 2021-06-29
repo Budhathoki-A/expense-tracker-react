65739629a37efc281accbd2ebc39ae0584c1ffc6
Amir Budhathoki <amirbudhathoki01@gmail.com>
2021-06-29 22:04:52 +1000

    global state introduced

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,1 +1,14 @@
 import React, { createContext, useReducer } from 'react';
+
+// Initial state
+const initailState = {
+  transactions: [
+    { id: 1, text: 'Salary', amount: 1500 },
+    { id: 2, text: 'Rent', amount: -250 },
+    { id: 3, text: 'Grocery', amount: -150 },
+    { id: 4, text: 'Expenses', amount: -180 },
+  ],
+};
+
+//  Create Context
+export const GlobalContext = createContext(initailState);
