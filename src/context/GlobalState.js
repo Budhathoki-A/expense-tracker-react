2ced0d56a379a89dd3139854c7253469bdd6bef1
Amir Budhathoki <amirbudhathoki01@gmail.com>
2021-07-07 21:23:39 +1000

    error fixing with the empty initialized array object

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,17 +4,16 @@ import AppReducer from './AppReducer';
 // Initial state
 const initialState = {
   transactions: [
-    { id: 1, text: 'Salary', amount: 1500 },
     { id: 2, text: 'Rent', amount: -250 },
     { id: 3, text: 'Grocery', amount: -150 },
     { id: 4, text: 'Expenses', amount: -180 },
   ],
 };
 
-//  Create Context
+// Create context
 export const GlobalContext = createContext(initialState);
 
-// Provider for other components yo have access to our global state
+// Provider component
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
